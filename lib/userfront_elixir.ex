75392a5367537f52b9b632fa3f2dfa6dccf28945
fabianskier <fabianskier@icloud.com>
2023-01-07 20:19:19 +0100

    first commit

---
+++ lib/userfront_elixir.ex
@@ -0,0 +1,18 @@
+defmodule UserfrontElixir do
+  @moduledoc """
+  Documentation for `UserfrontElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UserfrontElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/userfront_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule UserfrontElixirTest do
+  use ExUnit.Case
+  doctest UserfrontElixir
+
+  test "greets the world" do
+    assert UserfrontElixir.hello() == :world
+  end
+end
